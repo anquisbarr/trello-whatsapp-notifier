e033d15f650b45cb11c281910905a8543ba4ef1b
billi.tsx <sebastian.quispe.b@utec.edu.pe>
2024-08-14 13:08:33 -0500

    feat: added filtered tasks from specified lists due this week

--- utils/task-filters.ts
+++ utils/task-filters.ts
@@ -7,7 +7,6 @@ export function filterTasksForToday(tasks: TrelloBoardElement[]) {
 			if (card.due) {
 				const dueDate =
 					typeof card.due === "string" ? parseISO(card.due) : card.due;
-				console.log(`Checking date: ${dueDate} for task: ${card.name}`);
 				return isToday(dueDate);
 			}
 			return false;
@@ -21,10 +20,39 @@ export function filterTasksForThisWeek(tasks: TrelloBoardElement[]) {
 			if (card.due) {
 				const dueDate =
 					typeof card.due === "string" ? parseISO(card.due) : card.due;
-				console.log(`Checking date: ${dueDate} for task: ${card.name}`);
 				return isThisWeek(dueDate, { weekStartsOn: 1 }); // Week starts on Monday
 			}
 			return false;
 		}),
 	);
+}
+
+// const listNamesToFilter = ["Tareas pendientes * fecha", "En proceso", "Hecho"];
+// const listNamesToFilter = ["Backlog", "To Do", "Doing"] as const;
+const listNamesToFilter = new Set(["Backlog", "To Do", "Doing"]);
+
+export function filterTasksFromListsForThisWeek(tasks: TrelloBoardElement[]) {
+	const groupedTasks = tasks.reduce((acc, list) => {
+		if (listNamesToFilter.has(list.listName)) {
+			const dueThisWeek = list.cards.filter((card) => {
+				if (card.due) {
+					const dueDate =
+						typeof card.due === "string" ? parseISO(card.due) : card.due;
+					return isThisWeek(dueDate, { weekStartsOn: 1 });
+				}
+				return false;
+			});
+
+			if (dueThisWeek.length > 0) {
+				acc.push({
+					id: list.id,
+					listName: list.listName,
+					cards: dueThisWeek,
+				});
+			}
+		}
+		return acc;
+	}, [] as TrelloBoardElement[]);
+
+	return groupedTasks;
 }
